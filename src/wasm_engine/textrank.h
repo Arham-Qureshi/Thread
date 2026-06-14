cfdb68055862ef2de004cf4e143a1cf70e51f34b
Arham-Qureshi <arhamqureshi0098@gmail.com>
2026-06-15 00:32:44 +0530

    header file for text rank so it can be imported to and with stemmer algo

---
+++ src/wasm_engine/textrank.h
@@ -0,0 +1,12 @@
+#ifndef THREAD_TEXTRANK_H
+#define THREAD_TEXTRANK_H
+
+#include <string>
+#include <vector>
+
+namespace textrank {
+
+std::vector<std::string> extractKeywords(const std::string &text);
+
+}
+#endif
